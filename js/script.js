0dbd03f1b360971c52704f556373ca36f4bef048
meydson <64757536+meydson@users.noreply.github.com>
2020-05-28 23:54:13 -0300

    ajustando layout

--- js/script.js
+++ js/script.js
@@ -12,13 +12,13 @@ let food = {
     y: Math.floor(Math.random() * 15 + 1) * box
 }
 function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "lightblue";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
     for (i = 0; i < snake.length; i++) {
-        context.fillStyle = "green";
+        context.fillStyle = "blue";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
