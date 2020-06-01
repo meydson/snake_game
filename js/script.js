c30f1b37ee4509df9b3a4762a3c7950457bf9455
meydson <64757536+meydson@users.noreply.github.com>
2020-06-01 02:17:24 -0300

    adicionada as instruções

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ function iniciarJogo() {
     for (i =  1; i < snake.length; i++){
         if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
-            alert("Game Over! Seu recorde foi = " + score);
+            alert("Game Over! Seu recorde foi de " + score + " frutas");
         }
     }
     criarBG();
